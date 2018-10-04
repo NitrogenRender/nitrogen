7c43a0a57f33798bb0a1848c753854b6f298dbb0
Thomas Herzog <thomas.herzog@simedis.com>
2018-10-04 16:34:40 +0200

    added C API and initial setup

--- src/bindings/mod.rs
+++ src/bindings/mod.rs
@@ -1,5 +1,31 @@
+use std::os::raw::*;
+use std::ffi::CStr;
+
+#[repr(C)]
+pub struct BindingCreationInfoX11 {
+    name: *const c_char,
+    version: u32,
+    display: *mut c_void,
+    window: c_ulong,
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn give_me_five() -> u64 {
     println!("High five!");
     5
+}
+
+#[no_mangle]
+pub extern "C" fn setup_x11(info: &BindingCreationInfoX11) {
+    use std::mem::transmute;
+    let info = unsafe {
+        super::CreationInfoX11 {
+            name: CStr::from_ptr(info.name).to_string_lossy().to_string(),
+            window: transmute(info.window),
+            display: transmute(info.display),
+            version: info.version,
+        }
+    };
+
+    super::setup_x11(info);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,32 @@
+extern crate gfx_hal as gfx;
+extern crate gfx_backend_vulkan as back;
+
+extern crate ash;
+
 pub mod bindings;
+
+use gfx::Instance;
+
+use ash::vk;
+
+use std::os::raw::*;
+
+pub struct CreationInfoX11 {
+    name: String,
+    version: u32,
+    display: *mut vk::Display,
+    window: vk::Window,
+}
+
+pub fn setup_x11(info: CreationInfoX11) {
+
+
+    let instance = back::Instance::create(&info.name, info.version);
+    let surface = instance.create_surface_from_xlib(info.display, info.window);
+    let adapters = instance.enumerate_adapters();
+
+    for adapter in adapters {
+        println!("{:?}", adapter.info);
+    }
+}
 
